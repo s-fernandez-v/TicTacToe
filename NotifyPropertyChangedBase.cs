0ac999ce6a7ff846149b27c07c4e31a844926796
sfernandez <sfernandez@noesisengine.com>
2018-01-02 20:56:24 +0100

    TicTacToe solution with logic and basic UI created

---
+++ NotifyPropertyChangedBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TicTacToe
+{
+    class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
